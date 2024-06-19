cd9593228575c4200d513bffccc407b2d1cb2f36
Aryan6043 <aryanyadav98and16@gmail.com>
2024-06-19 18:09:25 +0530

    4. Use of Constructor, Setter and Field Injection

--- spring-core-concepts/src/main/java/com/lcwd/core/couple/Person.java
+++ spring-core-concepts/src/main/java/com/lcwd/core/couple/Person.java
@@ -6,10 +6,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-
-    @Autowired // Field injection is not recommended but just for an example
-    @Qualifier("dog")
+    @Autowired // Field Injection [Highly Readable]
+    @Qualifier("mouse")
     Animal animal;
+
+    @Autowired // Constructor Injection [Used for mandatory dependencies]
+    public Person(@Qualifier("dog") Animal animal) {
+        System.out.println("Constructor called...");
+        this.animal = animal;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    @Autowired // Setter Injection [Used for optional dependencies]
+    public void setAnimal(@Qualifier("cat") Animal animal) {
+        System.out.println("Setter method called...");
+        this.animal = animal;
+    }
 
     public void playWithAnimal() {
         // using animal
